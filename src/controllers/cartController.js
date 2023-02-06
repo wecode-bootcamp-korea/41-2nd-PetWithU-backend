5e80440d870f630ae318059c9a716ca027415bda
Judy-Choi <mjchoi0831@gmail.com>
2023-02-07 01:50:41 +0900

    장바구니 조회 API

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,5 +1,6 @@
 const cartService = require("../services/cartService");
 const { asyncErrorHandler } = require("../utils/errorHandling");
+
 // 1. 장바구니 상품 추가 / 수량 변경 (주문수량 증가/감소)
 const upsertCart = asyncErrorHandler(async (req, res) => {
   const { productId, quantity } = req.body;
@@ -11,6 +12,13 @@ const upsertCart = asyncErrorHandler(async (req, res) => {
   return res.status(201).json({ message: "UPDATE_CART_SUCCESS" });
 });
 
+// 2. 장바구니 상품 조회
+const readCart = asyncErrorHandler(async (req, res) => {
+  cartList = await cartService.readCart(req.userId);
+  return res.status(200).json(cartList);
+});
+
 module.exports = {
   upsertCart,
+  readCart,
 };
