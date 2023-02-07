75de9163594a00dbb7746d9c2e3fb393e7d7a8a6
Judy-Choi <mjchoi0831@gmail.com>
2023-02-08 00:00:11 +0900

    산책로 스크랩 모아보기

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const userRouter = require("./userRouter");
 const productRouter = require("./productRouter");
 const communityRouter = require("./communityRouter");
+const cartRouter = require("./cartRouter");
 const promenadeRouter = require("./promenadeRouter");
 const cartRouter = require("./cartRouter");
 
 router.use("/users", userRouter.router);
 router.use("/products", productRouter.router);
 router.use("/community", communityRouter.router);
+router.use("/carts", cartRouter.router);
 router.use("/promenade", promenadeRouter.router);
 router.use("/carts", cartRouter.router);
 
