415f6153b1b439be53592ba5db21cee69f95696f
Judy-Choi <mjchoi0831@gmail.com>
2023-02-09 10:04:10 +0900

    라우터 버그 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const userRouter = require("./userRouter");
 const productRouter = require("./productRouter");
 const communityRouter = require("./communityRouter");
 const promenadeRouter = require("./promenadeRouter");
+const cartRouter = require("./cartRouter");
 
 router.use("/users", userRouter.router);
 router.use("/products", productRouter.router);
 router.use("/community", communityRouter.router);
 router.use("/promenade", promenadeRouter.router);
+router.use("/carts", cartRouter.router);
 
 module.exports = router;
